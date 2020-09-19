8eeb6d22006222e8fe5951c4f93e57888f99d52d
Brenden Forbis <brenden.forbis@outlook.com>
2020-09-19 13:54:21 -0400

    Added the searchAmazon function to run the API call

--- client/src/components/Keyboard/index.js
+++ client/src/components/Keyboard/index.js
@@ -1,28 +1,58 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import API from "../../utils/API";
-import Cards from "../../pages/Cards";
+import Menu from "../Menu";
+import WalmartCard from "../../pages/WalmartCard";
+import AmazonCard from "../../pages/AmazonCard";
 
 class Keyboard extends Component {
     state = {
-        results: {},
+        results: [],
+        amazonResults: [],
     };
 
     // When this component mounts, search for the item "keyboard"
     componentDidMount() {
         this.searchItems("keyboard");
+        this.searchAmazon("computerkeyboard");
     }
 
     searchItems = query => {
         API.searchItems(query)
-            .then(res => this.setState({ results: res.data }))
+            .then(res => this.setState({results: res.data.items}))
+            .catch(err => console.log(err));
+    };
+
+    searchAmazon = keyword => {
+        API.searchAmazon(keyword)
+            .then(res => this.setState({amazonResults: res.data}))
             .catch(err => console.log(err));
     };
 
 
     render() {
         return (
-            <Cards/>
-        );
+            <div className="text-center mb-32">
+                <h3 className="text-primary text-5xl">We search it for you</h3>
+                <Menu/>
+                <main className="mt-5">
+                    <div className="flex flex-wrap justify-center">
+                        {this.state.results.map(item => {
+                            return (
+                                <WalmartCard results={item} key={item.itemId} addFavorites={this.props.addFavorites}/>
+                            )
+                        })}
+                    </div>
+                    <div className="flex flex-wrap justify-center">
+                        {this.state.amazonResults.map(item => {
+                            return( 
+                                <AmazonCard amazonResults= {item} key={item.ASIN} addFavorites={this.props.addFavorites}/>
+                            )
+                        })}
+                    </div>
+                    
+                </main>
+            </div>
+        )
     }
 }
 
