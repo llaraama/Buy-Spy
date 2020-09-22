cc3270d819b53b43d0a2e69b2356d78ddfcc6be1
Laura Lara <llaraama@uncc.edu>
2020-09-22 15:57:06 -0400

    database favorites table is now able to render in card on the favorites page everytime the user clicks on the heart button they will be able to see it in their favorites section, the pictures are not rendering correctly working on that issue

--- client/src/pages/Favorites/index.js
+++ client/src/pages/Favorites/index.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, {Component} from "react";
 import WalmartCard from "../WalmartCard"
+import API from "../../utils/API"
 
 
-function Favorites() {
-   return(
-    <div>
-   
-</div>
-   )
-
-
+class Favorites extends Component{
   
+state={
+    dbResults:[]
+
+};
+componentDidMount(){
+    API.getFavorites()
+        .then(data =>{
+            console.log("this is it ")
+            console.log(data.data);
+            this.setState({ dbResults: data.data})
+        })
+        .catch (err=> console.log(err))
+}
+render(){
+    return (
+        <div className="text-center mb-32">
+            <main className="mt-5">
+                <div className="flex flex-wrap justify-center">
+                    {this.state.dbResults.map(item => {
+                        return (
+                            <WalmartCard 
+                            results={item} 
+                            key={item.itemId} 
+                         />
+                        )
+                    })}
+                </div>
+           
+            </main>
+        </div>
+    )
+}
 }
 
 export default Favorites;
